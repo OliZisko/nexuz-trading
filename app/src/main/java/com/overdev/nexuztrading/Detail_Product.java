5b43a123e5d47e9750611127d9a0808280aff21b
Alberto Garcia Cerruto <albertogarciacerruto@gmail.com>
2019-05-11 09:41:39 -0400

    Vista del detalle con su variante horizontal

---
+++ app/src/main/java/com/overdev/nexuztrading/Detail_Product.java
@@ -0,0 +1,13 @@
+package com.overdev.nexuztrading;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detail_Product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail__product);
+    }
+}
